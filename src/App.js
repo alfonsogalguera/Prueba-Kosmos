07558e01bfcaa6a8009d0c2a3706deb5c341fea0
DevAlfonso <57268703+alfonsogalguera@users.noreply.github.com>
2022-04-29 14:18:39 -0500

    primero
    primero

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import './assets/css/styles.css'
+import Formulario from './components/Formulario';
+
+function App() {
+  return (
+    <Formulario/>
+  );
+}
+
+export default App;
